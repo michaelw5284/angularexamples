f68e858b56d9e1a83385e0b9b9f1325658ff5e14
Michael Walter <Michael.Walter@moat.co.uk>
2017-09-17 20:27:46 +0100

    Added input selector

--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-favorite',
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavoriteComponent implements OnInit {
 
+  @Input() isSelected : boolean = false;
   constructor() { }
 
   ngOnInit() {
+  }
+  toggleSelected(){
+    this.isSelected = !this.isSelected;
   }
 
 }
