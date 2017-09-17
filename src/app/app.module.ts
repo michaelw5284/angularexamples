0fb18f5b5d4f1a6ece5014cb78c554a436d0fd0f
Michael Walter <Michael.Walter@moat.co.uk>
2017-09-17 17:10:58 +0100

    More examples

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { HelpdeskService } from './helpdesk/helpdesk.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CallsComponent } from './calls/calls.component';
@@ -12,7 +13,8 @@ import { CallsComponent } from './calls/calls.component';
     CallsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    FormsModule
   ],
   providers: [HelpdeskService],
   bootstrap: [AppComponent]
--- src/app/calls/calls.component.ts
+++ src/app/calls/calls.component.ts
@@ -11,20 +11,34 @@ export class CallsComponent implements OnInit {
   constructor(private helpdeskService : HelpdeskService) { }
 
   calls : string[];
+  defaultOneWayBinding : string = "me@home.com";
+  modelBoundValue : string  = "Test";
  
   ngOnInit() {
   
       this.calls = this.helpdeskService.getCalls();
-      
-
   }
 
   itemSelected(i){
   
     alert("You selected item " + i);
-
-
   }
+  onKeyUp($event){
+    if($event.keyCode === 13){
+
+        alert($event.target.value);
+    }
+  }
+
+  onKeyUpFiltered(email){
+
+          alert(email);
+  }
+
+  onKeuUpWithModelBinding(){
+   alert(this.modelBoundValue);
+  }
+
   
 
 }
